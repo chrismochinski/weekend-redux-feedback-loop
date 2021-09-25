fe08a6409b42389eddbda9ebe9912f618cacaa23
Chris "Mo" Mochinski <cmochinski@gmail.com>
2021-09-25 18:14:00 -0500

    Cleaned up error on empty values

--- src/components/ReviewPage5/ReviewPage5.jsx
+++ src/components/ReviewPage5/ReviewPage5.jsx
@@ -10,21 +10,31 @@ function Review() {
 
     console.log('store is now:', reduxStore);
 
-    const postToDatabase = () => {
+    const postToDatabase = () => { //conditional to avoid errors - start over if any values missing
         console.log('Info to database:', reduxStore);
-        axios({ 
-            method: 'POST',
-            url: '/feedback',
-            data: reduxStore,
-        }).then(response => {
-            finalPage(); // call function below to send to "thank you" page
-        }).catch((error) => {
-            console.log('error in client-side confirm page:', error)
-        });
+        if (reduxStore.feeling === '' || reduxStore.understanding === '' || reduxStore.support === '' || reduxStore.comments === '') {
+            alert("You are missing info. Let's start over.")
+            startOver();
+        }
+        else {
+            axios({
+                method: 'POST',
+                url: '/feedback',
+                data: reduxStore,
+            }).then(response => {
+                finalPage(); // call function below to send to "thank you" page
+            }).catch((error) => {
+                console.log('error in client-side confirm page:', error)
+            });
+        };
     };
 
     const finalPage = () => {
         history.push('/thankyou');
+    }
+
+    const startOver = () => {
+        history.push('/');
     }
 
 
@@ -35,7 +45,7 @@ function Review() {
 
             <h3>Feeling: {reduxStore.feeling}</h3>
             <h3>Understanding: {reduxStore.understanding}</h3>
-            <h3>Support: {reduxStore.understanding}</h3>
+            <h3>Support: {reduxStore.support}</h3>
             <h3>Comments: {reduxStore.comments}</h3>
 
             <button onClick={() => postToDatabase(reduxStore)}>Confirm</button>
